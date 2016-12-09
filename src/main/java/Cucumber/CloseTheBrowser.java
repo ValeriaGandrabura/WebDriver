bbcc6cb4753fd6c14a83a6b1edc4aba88e7a2519
ValeriaGandrabura <vgandrabura@yahoo.com>
2016-12-09 13:00:57 +0200

    Second  Commit

--- src/main/java/Cucumber/CloseTheBrowser.java
+++ src/main/java/Cucumber/CloseTheBrowser.java
@@ -1,14 +1,18 @@
 package Cucumber;
 
+import org.openqa.selenium.WebDriver;
+
 import static Cucumber.OpenWikiSearchPage.getWebDriver;
 
 public class CloseTheBrowser {
+
+    private static WebDriver driver= null;
 
     public static void closePage() {
         getWebDriver().close();
     }
     public void closeSession () {
-        OpenWikiSearchPage.getWebDriver() = null;
+        getWebDriver().close();
     }
 
 
